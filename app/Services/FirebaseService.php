a42a463e3af5d96ac468a5d3c8c70cb90f65af27
Sergio <lopezdelafuentesergio352@gmail.com>
2025-06-13 19:45:28 +0200

    5

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -7,18 +7,26 @@ use Kreait\Firebase\Auth;
 
 class FirebaseService
 {
-    protected Auth $auth;
+    protected ?Auth $auth = null;
 
-    public function __construct()
+    protected function getAuth(): Auth
     {
-        $factory = (new Factory)
-            ->withServiceAccount(storage_path('app\firebase\gestion-de-proyectos-271ca-firebase-adminsdk-fbsvc-08599b1cb5.json'));
+        if (!$this->auth) {
+            $serviceAccountPath = storage_path('app/firebase/gestion-de-proyectos-271ca-firebase-adminsdk-fbsvc-08599b1cb5.json');
 
-        $this->auth = $factory->createAuth();
+            if (!file_exists($serviceAccountPath)) {
+                throw new \Exception("Firebase service account file not found at: $serviceAccountPath");
+            }
+
+            $factory = (new Factory)->withServiceAccount($serviceAccountPath);
+            $this->auth = $factory->createAuth();
+        }
+
+        return $this->auth;
     }
 
     public function getAllUsers()
     {
-        return $this->auth->listUsers();
+        return $this->getAuth()->listUsers();
     }
 }
